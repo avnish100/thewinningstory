bbd72e331cd9c10582465683ecac7fca6965343a
Avnish Jha <avnishjha1005@gmail.com>
2025-05-18 15:51:15 +0530

    merge:main

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
-const config = {
-  plugins: ["@tailwindcss/postcss"],
-=======
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
     tailwindcss: {},
   },
->>>>>>> main
 };
 
 export default config;
